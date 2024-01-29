53a5368b7b0ee774f734876e8256da2a541a4652
Alex <alexandrethiry999@gmail.com>
2024-01-29 09:52:39 -0500

    basic pyxel interface

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import pyxel
+
+pyxel.init(400,400)
+
+def update():
+    if pyxel.btnp(pyxel.KEY_Q):
+        pyxel.quit()
+
+def draw():
+    pyxel.cls(0)
+    pyxel.rect(10, 10, 20, 20, 11)
+
+pyxel.run(update, draw)
