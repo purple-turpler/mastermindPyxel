a7a3b670cedb27495c417b7ab20aafd08a1289e2
Alex <alexandrethiry999@gmail.com>
2024-01-29 10:34:59 -0500

    feat: switched to using a class

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import pyxel
+class App:
+    def __init__(self):
+        pyxel.init(400,400, title="Mastermind", fps=15, quit_key=pyxel.KEY_Q)
+        self.x = 0
+        pyxel.run(self.update, self.draw)
 
-pyxel.init(400,400)
+    def update(self):
+        self.x = (self.x + 1) % pyxel.width
 
-def update():
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.rect(self.x, 0, 8, 8, 9)
 
-def draw():
-    pyxel.cls(0)
-    pyxel.rect(10, 10, 20, 20, 11)
-
-pyxel.run(update, draw)
+App()
