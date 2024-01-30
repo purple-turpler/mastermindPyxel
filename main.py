2bd497ba1e3bf6d9159be581367e19820235fc9e
Alex <alexandrethiry999@gmail.com>
2024-01-30 10:05:47 -0500

    feat: added simple inteface

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import pyxel
 class App:
     def __init__(self):
-        pyxel.init(400,400, title="Mastermind", fps=15, quit_key=pyxel.KEY_Q)
-        self.x = 0
-        pyxel.run(self.update, self.draw)
+        pyxel.init(600, 700, title="Mastermind", fps=60, quit_key=pyxel.KEY_Q) #Parameters for screen
+        self.colorReference = {
+            "grey": 13,
+            "white": 7,
+            "blue": 12,
+            "yellow": 10,
+            "orange": 9,
+            "purple": 2,
+            "red": 8,
+            "green": 11}
+        self.gameStatus = 0
+        self.password = []
+        self.currentGuess = []
+        self.prevousGuesses = []
+        pyxel.run(self.update, self.draw) #Run function that takes
+
+    def displayInterface(self, prevousGuesses, guess):
+        pyxel.text(pyxel.width/2 - 25,10,"MASTERMIND",7)
+        pyxel.rect(0,0,pyxel.width*0.15,pyxel.height,13)
+        pyxel.rect(pyxel.width-(pyxel.width*0.15),0,pyxel.width*0.15,pyxel.height,13)
 
     def update(self):
-        self.x = (self.x + 1) % pyxel.width
+        pass
 
     def draw(self):
-        pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
+        pyxel.cls(1) #Clear screen
+        self.displayInterface(self.prevousGuesses,self.currentGuess)
 
 App()
