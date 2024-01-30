4a7322243e377657bc799da5a3459c9a9f75d542
Alex <alexandrethiry999@gmail.com>
2024-01-30 14:57:30 -0500

    feat: UI upgrade + conditional rendering for guesses

--- main.py
+++ main.py
@@ -13,20 +13,39 @@ class App:
             "green": 11}
         self.gameStatus = 0
         self.password = []
-        self.currentGuess = []
-        self.prevousGuesses = []
+        self.currentGuess = [0,0,0,0]
+        self.guesses = [[8,11,2,12],[7,7,11,8],[0,0,2,0]]
+        self.status = 10
         pyxel.run(self.update, self.draw) #Run function that takes
 
-    def displayInterface(self, prevousGuesses, guess):
-        pyxel.text(pyxel.width/2 - 25,10,"MASTERMIND",7)
+    def displayInterface(self):
+        #Basic UI
         pyxel.rect(0,0,pyxel.width*0.15,pyxel.height,13)
-        pyxel.rect(pyxel.width-(pyxel.width*0.15),0,pyxel.width*0.15,pyxel.height,13)
+        pyxel.rect(pyxel.width-(pyxel.width*0.15), 0, pyxel.width*0.15, pyxel.height, 13)
+        pyxel.rect(pyxel.width*0.15, pyxel.height-100, pyxel.width*0.7,100, 0)
+        pyxel.rect(pyxel.width*0.15, 0, pyxel.width*0.7, 100, 0)
+        pyxel.text(pyxel.width/2 - 25,10,"MASTERMIND",7)
+
+        #Custom UI based on previous guesses => conditional rendering
+        currentPos = 0
+        colorSwitch = False
+        for guess in self.guesses:
+            if self.status >= 0: 
+                print(guess)
+                self.status-=1
+            pyxel.rect(pyxel.width*0.15, pyxel.height-(150+currentPos), pyxel.width*0.7, 50, 5+colorSwitch)
+            xPos = 75
+            for color in guess:
+                pyxel.rect(pyxel.width*0.15 + xPos, pyxel.height-(150+currentPos-12.5), 25, 25, color)
+                xPos += 75
+            currentPos+=50
+            colorSwitch = not colorSwitch
 
     def update(self):
         pass
 
     def draw(self):
         pyxel.cls(1) #Clear screen
-        self.displayInterface(self.prevousGuesses,self.currentGuess)
+        self.displayInterface()
 
 App()
